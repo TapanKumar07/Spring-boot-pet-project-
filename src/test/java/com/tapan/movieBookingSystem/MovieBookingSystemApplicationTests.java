63b59b7854305f5ae839736406f56b477761a7ad
Tapan Kumar <tapan.kumar.in@freecharge.com>
2023-03-14 21:00:22 +0530

    Spring boot starter

---
+++ src/test/java/com/tapan/movieBookingSystem/MovieBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tapan.movieBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
